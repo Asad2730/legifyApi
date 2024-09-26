322591d45bbfb7a816a332987cb8c82cb2d686ca
Asad2730 <asadsajjad777@gmail.com>
2024-09-26 11:20:10 +0500

    swagger setup...

--- src/middlewares/expressMiddleware.ts
+++ src/middlewares/expressMiddleware.ts
@@ -1,12 +1,13 @@
 import cors from "cors";
 import express, { type Express } from "express";
 import router from "../routes/main";
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from "../config/swagger";
 
 const app: Express = express();
 
 app.use(cors())
 app.use(express.json())
+app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerSpec))
 app.use('/api',router)
-
-
 export default app ;
